a192f4e964d68f00be919fa76abf2ca1e1323799
clement_besnier <clemsciences@aol.com>
2018-06-08 16:10:47 +0200

    Début !

---
+++ app/src/main/java/static_bike_analyzer/MainActivity.java
@@ -0,0 +1,184 @@
+package static_bike_analyzer;
+
+import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.github.anastr.speedviewlib.SpeedView;
+import com.jjoe64.graphview.DefaultLabelFormatter;
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.GridLabelRenderer;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+
+public class MainActivity  extends BlunoLibrary{
+	private Button buttonScan;
+	private Button buttonSerialSend;
+	private EditText serialSendText;
+	private TextView serialReceivedText;
+	private SpeedView speedView;
+	private GraphView graph;
+	private LineGraphSeries<DataPoint> series;
+	private double graph2LastXValue ;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+        onCreateProcess();														//onCreate Process by BlunoLibrary
+
+
+        serialBegin(115200);													//set the Uart Baudrate on BLE chip to 115200
+
+        serialReceivedText=(TextView) findViewById(R.id.serialReveicedText);	//initial the EditText of the received data
+        serialSendText=(EditText) findViewById(R.id.serialSendText);			//initial the EditText of the sending data
+		speedView = (SpeedView) findViewById(R.id.speedView);
+
+		graph = (GraphView) findViewById(R.id.graph);
+		//ne marche pas
+		graph.getGridLabelRenderer().setLabelFormatter(new DefaultLabelFormatter()
+        {
+            @Override
+            public String formatLabel(double value, boolean isValueX) {
+                if (isValueX) {
+                    // show normal x values
+                    return super.formatLabel(value/10, true)+"s";
+                } else {
+                    // show currency for y values
+                    return super.formatLabel(value, false);
+                }
+            }
+        });
+        graph.getViewport().setXAxisBoundsManual(true);
+		graph.getViewport().setYAxisBoundsManual(true);
+		graph.getViewport().setMinX(0);
+		graph.getViewport().setMaxX(400);
+		graph.getViewport().setMinY(0);
+		graph.getViewport().setMaxY(100);
+		graph.getGridLabelRenderer().setGridStyle(GridLabelRenderer.GridStyle.HORIZONTAL);
+		series = new LineGraphSeries<>(new DataPoint[] {
+				new DataPoint(0, 1),
+				new DataPoint(1, 5),
+				new DataPoint(2, 3),
+				new DataPoint(3, 2),
+				new DataPoint(4, 6)
+		});
+		series.setDrawBackground(true);
+
+		graph.addSeries(series);
+		graph2LastXValue = 4d;
+
+
+        buttonSerialSend = (Button) findViewById(R.id.buttonSerialSend);		//initial the button for sending the data
+        buttonSerialSend.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+
+				serialSend(serialSendText.getText().toString());				//send the data to the BLUNO
+			}
+		});
+
+        buttonScan = (Button) findViewById(R.id.buttonScan);					//initial the button for scanning the BLE device
+        buttonScan.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+
+				buttonScanOnClickProcess();										//Alert Dialog for selecting the BLE device
+			}
+		});
+	}
+
+	protected void onResume(){
+		super.onResume();
+		System.out.println("BlUNOActivity onResume");
+		onResumeProcess();														//onResume Process by BlunoLibrary
+	}
+	
+	
+	
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		onActivityResultProcess(requestCode, resultCode, data);					//onActivityResult Process by BlunoLibrary
+		super.onActivityResult(requestCode, resultCode, data);
+	}
+	
+    @Override
+    protected void onPause() {
+        super.onPause();
+        onPauseProcess();														//onPause Process by BlunoLibrary
+    }
+	
+	protected void onStop() {
+		super.onStop();
+		onStopProcess();														//onStop Process by BlunoLibrary
+	}
+    
+	@Override
+    protected void onDestroy() {
+        super.onDestroy();	
+        onDestroyProcess();														//onDestroy Process by BlunoLibrary
+    }
+
+	@Override
+	public void onConectionStateChange(connectionStateEnum theConnectionState) {//Once connection state changes, this function will be called
+		switch (theConnectionState) {											//Four connection state
+		case isConnected:
+			buttonScan.setText("Connected");
+			break;
+		case isConnecting:
+			buttonScan.setText("Connecting");
+			break;
+		case isToScan:
+			buttonScan.setText("Scan de nouveau");
+			mConnectionState=connectionStateEnum.isScanning;
+			onConectionStateChange(mConnectionState);
+			scanLeDevice(true);
+			mScanDeviceDialog.show();
+			break;
+		case isScanning:
+			buttonScan.setText("Scanning");
+			break;
+		case isDisconnecting:
+			buttonScan.setText("isDisconnecting");
+			break;
+		default:
+			break;
+		}
+	}
+
+	@Override
+	public void onSerialReceived(String theString) {							//Once connection data received, this function will be called
+
+		// TODO Auto-generated method stub
+		int v;
+		serialReceivedText.setText(theString);							//append the text into the EditText
+
+		try
+		{
+			v = Integer.parseInt(theString);
+			//System.out.println (num);
+			//SpeedView speedView = (SpeedView) findViewById(R.id.speedView);
+			speedView.setWithTremble(false);
+			speedView.speedTo(v,0);
+
+			graph2LastXValue += 1d;
+			series.appendData(new DataPoint(graph2LastXValue, v), true, 400);
+		}catch(NumberFormatException e)
+		{
+			System.out.println("La chaine de caractères n'est pas un nombre parsable!!");
+		}
+		;
+
+		//The Serial data from the BLUNO may be sub-packaged, so using a buffer to hold the String is a good choice.
+		//((ScrollView)serialReceivedText.getParent()).fullScroll(View.FOCUS_DOWN);
+	}
+
+}
